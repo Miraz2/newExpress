906da71a95abbae39309348e17d2dbd935cf7e42
Mirazul Momen Miraz <miraz200416@gmail.com>
2024-05-24 16:35:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (_req, res) {
+  res.send("Hello World");
+});
+app.get("/about", function (_req, res) {
+  res.send("About Page");
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening ${port}...`));
